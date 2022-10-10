c9800e73034af2b106b46abb8c981445aef1e4b1
F-a-y-t-h <14098907+F-a-y-t-h@users.noreply.github.com>
2022-10-10 22:59:46 +0100

    Update NPCKillCounterPlayer.cs

--- NPCKillCounterPlayer.cs
+++ NPCKillCounterPlayer.cs
@@ -1,48 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using Mono.Cecil.Cil;
-using MonoMod.Cil;
-using Terraria;
-using Terraria.DataStructures;
-using Terraria.ModLoader;
-using Terraria.ModLoader.Config;
-using Terraria.ModLoader.IO;
-
-namespace NPCKillCounter;
-
-public class NPCKillCounterPlayer : ModPlayer
-{
-    internal static int Hit;
-    internal static readonly DefaultDictionary<string, int> Count = new(); // 注意:非静态readonly需要在IL中实时获取.和本地客户端挂钩.
-
-    public override void Load()
-    {
-        IL.Terraria.Player.OnKillNPC += il => // 单人本地客户端/多人本地客户端.
-        {
-            var ilCursor = new ILCursor(il);
-            ilCursor.Emit(OpCodes.Ldarg_1); // NPCKillAttempt
-            ilCursor.Emit(OpCodes.Ldfld, typeof(NPCKillAttempt).GetField(nameof(NPCKillAttempt.npc)));
-            ilCursor.EmitDelegate<Action<NPC>>(npc => { Count[new NPCDefinition(npc.type).ToString()]++; });
-        };
-    }
-
-    public override void SaveData(TagCompound tag)
-    {
-        var data = new List<string>();
-        foreach (var (name, count) in Count)
-        {
-            data.Add($"{name}: {count}");
-        }
-
-        tag.Set(nameof(Count), data);
-    }
-
-    public override void LoadData(TagCompound tag)
-    {
-        Count.Clear();
-        if (tag.ContainsKey(nameof(Count)))
-        {
-            tag.Get<List<string>>(nameof(Count)).ForEach(obj =>using System;
+using System;
 using System.Collections.Generic;
 using Mono.Cecil.Cil;
 using MonoMod.Cil;
